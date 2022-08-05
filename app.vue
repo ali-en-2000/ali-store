b4448577182a1c5848f77ec1ce3f86376fabefd8
alireza mohammadi <a.rezamohmmadi369@gmail.com>
2022-08-05 10:37:41 +0430

    add firebase page

--- app.vue
+++ app.vue
@@ -8,4 +8,6 @@
 </script>
 <style>
 @import "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css";
+@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css";
+
 </style>
